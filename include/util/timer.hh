8b2acc0cda2fd422bea3fbb601f79f2e40bf0cad
L-LYR <lisicen97@gmail.com>
2022-09-09 13:47:58 +0800

    feat: add ClockTimer and TraceTimer

--- include/util/timer.hh
+++ include/util/timer.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <chrono>
+#include <vector>
 
 namespace toolbox {
 namespace util {
@@ -45,6 +46,51 @@ inline auto tscp() -> uint64_t {
   asm volatile("rdtscp" : "=a"(a), "=d"(d));
   return (d << 32) | a;
 }
+
+/// Notice:
+///    Before you use this timer, you shall check your cpu have constant_tsc flag
+///    by using command: `cat /proc/cpuinfo | grep constant_tsc`.
+class ClockTimer {
+ public:
+  ClockTimer() = default;
+  ~ClockTimer() = default;
+
+ public:
+  auto begin() -> void { begin_ = tscp(); }
+  auto end() -> void { end_ = tscp(); }
+  auto reset() -> void { begin_ = end_ = 0; }
+  auto elapsed() -> uint64_t { return end_ - begin_; }
+
+ private:
+  uint64_t begin_{};
+  uint64_t end_{};
+};
+
+class TraceTimer {
+ public:
+  TraceTimer() = default;
+  ~TraceTimer() = default;
+
+ private:
+  auto tick() -> void { trace_.emplace_back(clock::now()); }
+
+  auto reset() -> void { trace_.resize(0); }
+
+  auto nPeriod() -> size_t { return trace_.size(); }
+
+  template <typename T>
+  auto period(uint32_t i) -> uint64_t {
+    return std::chrono::duration_cast<T>(trace_.at(i + 1) - trace_.at(i)).count();
+  }
+
+  template <typename T>
+  auto elapsed() -> uint64_t {
+    return std::chrono::duration_cast<T>(trace_.back() - trace_.front()).count();
+  }
+
+ private:
+  std::vector<time_point> trace_;
+};
 
 }  // namespace util
 }  // namespace toolbox
